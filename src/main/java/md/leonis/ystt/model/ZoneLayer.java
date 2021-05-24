892078e373c8749496b9431c6973d94af0a6550a
Leonis <tv-games@mail.ru>
2021-05-24 19:33:50 +0300

    Simplify code

--- src/main/java/md/leonis/ystt/model/ZoneLayer.java
+++ src/main/java/md/leonis/ystt/model/ZoneLayer.java
@@ -2,22 +2,13 @@ package md.leonis.ystt.model;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
-
 public enum ZoneLayer {
 
-    BOTTOM(0),
-    MIDDLE(1),
-    TOP(2);
-
-    private final int id;
-
-    ZoneLayer(int id) {
-        this.id = id;
-    }
+    BOTTOM,
+    MIDDLE,
+    TOP;
 
     public static String byId(String id) {
-        return Arrays.stream(ZoneLayer.values()).filter(z -> z.id == Integer.parseInt(id)).findFirst()
-                .map(z -> StringUtils.capitalize(z.name().toLowerCase())).orElse(null);
+        return StringUtils.capitalize(ZoneLayer.values()[Integer.parseInt(id)].name().toLowerCase());
     }
 }
