edbfb684c561bd6f51a2f75fe4ac36545f828d72
Leonis <tv-games@mail.ru>
2022-11-15 21:48:54 +0200

    Update movement types

--- src/main/java/md/leonis/ystt/model/yodesk/characters/MovementType.java
+++ src/main/java/md/leonis/ystt/model/yodesk/characters/MovementType.java
@@ -5,19 +5,19 @@ import java.util.Map;
 
 public enum MovementType {
 
-    NONE(0),        // PLAYER
-    UNKNOWN1(1),    // ENEMY_HARD
-    UNKNOWN2(2),    // ENEMY_MILD
-    UNKNOWN3(3),    // ENEMY
-    SIT(4),         // ENEMY_SIT
-    UNKNOWN5(5),    // unused
-    UNKNOWN6(6),    // ENEMY_INACTIVE
-    UNKNOWN7(7),    // ENEMY_ROBOT
-    UNKNOWN8(8),    // ENEMY_ROBOT_MILD
-    WANDER(9),      // ENEMY_ROBOT_HARD
-    PATROL(10),     // ENEMY_PATROL
-    UNKNOWN11(11),  // ENEMY_SCARED
-    ANIMATION(12);  // MAP_ITEM
+    NONE(0),         // PLAYER
+    UNSPECIFIC1(1),  // ENEMY_HARD
+    UNSPECIFIC2(2),  // ENEMY_MILD
+    UNSPECIFIC3(3),  // ENEMY
+    SIT(4),          // ENEMY_SIT
+    UNUSED(5),       // UNUSED_INDY_ONLY
+    UNSPECIFIC4(6),  // ENEMY_INACTIVE
+    UNSPECIFIC5(7),  // ENEMY_ROBOT
+    DROID(8),        // ENEMY_ROBOT_MILD
+    WANDER(9),       // ENEMY_ROBOT_HARD
+    PATROL(10),      // ENEMY_PATROL
+    SCAREDY(11),     // ENEMY_SCARED
+    ANIMATION(12);   // MAP_ITEM
 
     private final long id;
 
